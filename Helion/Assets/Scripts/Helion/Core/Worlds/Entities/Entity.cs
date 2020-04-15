220c2d025db69c44f2b84025f097c85b85f3032d
Chris <38708476+wchrisk@users.noreply.github.com>
2020-04-15 19:05:21 -0400

    Added entity world component

--- Helion/Assets/Scripts/Helion/Core/Worlds/Entities/Entity.cs
+++ Helion/Assets/Scripts/Helion/Core/Worlds/Entities/Entity.cs
@@ -1,11 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Helion.Core.Worlds.Entities
 {
-    public class Entity
+    /// <summary>
+    /// Holds data for the entity in a world.
+    /// </summary>
+    public class Entity : MonoBehaviour, IDisposable
     {
-        private GameObject gameObject;
+        public Vector3 Velocity;
+        internal LinkedListNode<Entity> entityNode;
 
-        // TODO
+        public GameObject EntityGameObject => transform.parent.gameObject;
+
+        void Update()
+        {
+            // TODO
+        }
+
+        void FixedUpdate()
+        {
+            // TODO
+        }
+
+        public void Dispose()
+        {
+            Destroy(EntityGameObject);
+            entityNode.List.Remove(entityNode);
+        }
     }
 }
--- Helion/Assets/Scripts/Helion/Core/Worlds/Entities/EntityManager.cs
+++ Helion/Assets/Scripts/Helion/Core/Worlds/Entities/EntityManager.cs
@@ -1,4 +1,7 @@
-﻿using Helion.Core.Resource.Maps;
+﻿using System.Collections.Generic;
+using Helion.Core.Resource.Maps;
+using Helion.Core.Resource.Maps.Doom;
+using Helion.Core.Util;
 using Helion.Core.Util.Extensions;
 using UnityEngine;
 
@@ -6,12 +9,34 @@ namespace Helion.Core.Worlds.Entities
 {
     public class EntityManager
     {
-        private GameObject gameObject;
+        public readonly LinkedList<Entity> Entities = new LinkedList<Entity>();
+        private readonly GameObject entityCollectorGameObject;
 
         public EntityManager(GameObject parentObject, IMap map)
         {
-            gameObject = new GameObject("Entities");
-            parentObject.SetChild(gameObject);
+            entityCollectorGameObject = new GameObject("Entities");
+            parentObject.SetChild(entityCollectorGameObject);
+
+            CreateEntities(((DoomMap)map).Things);
+        }
+
+        private void CreateEntities(IEnumerable<DoomThing> things)
+        {
+            foreach (DoomThing thing in things)
+            {
+                GameObject entityObject = new GameObject($"Entity UNKNOWN ({thing.EditorNumber})");
+                entityCollectorGameObject.SetChild(entityObject);
+
+                // TODO: Need to use the BSP for the Y coordinate base, and decorate for the bbox.
+                // TODO: Note this is the center, not the Y-bottom center foot location.
+                // TODO: Does spawning this cause collision detection if it ends up at the origin?
+                BoxCollider collider = entityObject.AddComponent<BoxCollider>();
+                collider.center = new Vector3(thing.Position.x, 32, thing.Position.y) * Constants.MapUnit;
+                collider.size = new Vector3(32, 32, 32) * Constants.MapUnit;
+
+                Entity entity = entityObject.AddComponent<Entity>();
+                entity.entityNode = Entities.AddLast(entity);
+            }
         }
     }
 }
