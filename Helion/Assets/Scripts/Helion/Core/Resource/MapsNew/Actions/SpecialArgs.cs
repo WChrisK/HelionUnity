b9cfcce9b000588385c79d5c0c294a25c6ffff91
Chris <38708476+wchrisk@users.noreply.github.com>
2020-04-22 13:37:06 -0400

    Special args detached from array

--- Helion/Assets/Scripts/Helion/Core/Resource/MapsNew/Actions/SpecialArgs.cs
+++ Helion/Assets/Scripts/Helion/Core/Resource/MapsNew/Actions/SpecialArgs.cs
@@ -11,36 +11,57 @@ namespace Helion.Core.Resource.MapsNew.Actions
     {
         public const int NumArgs = 5;
 
-        public readonly int[] Args;
-
-        public int Arg0 => Args[0];
-        public int Arg1 => Args[1];
-        public int Arg2 => Args[2];
-        public int Arg3 => Args[3];
-        public int Arg4 => Args[4];
+        public int Arg0;
+        public int Arg1;
+        public int Arg2;
+        public int Arg3;
+        public int Arg4;
 
         public SpecialArgs(int arg0 = 0, int arg1 = 0, int arg2 = 0, int arg3 = 0, int arg4 = 0)
         {
-            Args = new[] { arg0, arg1, arg2, arg3, arg4 };
+            Arg0 = arg0;
+            Arg1 = arg1;
+            Arg2 = arg2;
+            Arg3 = arg3;
+            Arg4 = arg4;
         }
 
-        public SpecialArgs(IEnumerable<int> args)
+        public SpecialArgs(IEnumerable<int> args) : this()
         {
-            Args = new[] { 0, 0, 0, 0, 0 };
-
-            int count = 0;
+            int index = 0;
             foreach (int arg in args)
             {
-                Args[count++] = arg;
+                switch (index)
+                {
+                case 0:
+                    Arg0 = arg;
+                    break;
+                case 1:
+                    Arg1 = arg;
+                    break;
+                case 2:
+                    Arg2 = arg;
+                    break;
+                case 3:
+                    Arg3 = arg;
+                    break;
+                case 4:
+                    Arg4 = arg;
+                    break;
+                }
 
-                if (count >= NumArgs)
+                if (index == NumArgs)
                     break;
             }
         }
 
         public SpecialArgs(SpecialArgs other)
         {
-            Args = other.Args.ToArray();
+            Arg0 = other.Arg0;
+            Arg1 = other.Arg1;
+            Arg2 = other.Arg2;
+            Arg3 = other.Arg3;
+            Arg4 = other.Arg4;
         }
     }
 }
