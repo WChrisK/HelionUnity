90f6ee41218672eccc4d657d4b11a917bf5db2b0
Chris <38708476+WChrisK@users.noreply.github.com>
2020-05-04 20:09:19 -0400

    Added accurate subsector intersection detection method

--- Helion/Assets/Scripts/Helion/Util/Geometry/Boxes/Box3F.cs
+++ Helion/Assets/Scripts/Helion/Util/Geometry/Boxes/Box3F.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using Helion.Util.Extensions;
 using Helion.Util.Geometry.Vectors;
@@ -9,35 +10,41 @@ namespace Helion.Util.Geometry.Boxes
     /// <summary>
     /// A two dimensional box, which follows the cartesian coordinate system.
     /// </summary>
-    public struct Box3D
+    public struct Box3F
     {
         /// <summary>
         /// The minimum point in the box. This is equal to the bottom left
         /// corner.
         /// </summary>
-        public Vec3D Min;
+        public Vec3F Min;
 
         /// <summary>
         /// The maximum point in the box. This is equal to the top right
         /// corner.
         /// </summary>
-        public Vec3D Max;
+        public Vec3F Max;
 
         /// <summary>
         /// The top value of the box.
         /// </summary>
-        public double Top => Max.Z;
+        public float Top => Max.Z;
 
         /// <summary>
         /// The bottom value of the box.
         /// </summary>
-        public double Bottom => Min.Z;
+        public float Bottom => Min.Z;
 
         /// <summary>
         /// Calculates the sides of this bounding box.
         /// </summary>
         /// <returns>The sides of the bounding box.</returns>
-        public Vec3D Sides => Max - Min;
+        public Vec3F Sides => Max - Min;
+
+        /// <summary>
+        /// Creates an XZ (birds eye view) box from this box.
+        /// </summary>
+        /// <returns>The box with the XZ coordinates.</returns>
+        public Box2F XZ => new Box2F(Min.XZ, Max.XZ);
 
         /// <summary>
         /// Creates a box from a bottom left and top right coordinate. It is an
@@ -49,8 +56,8 @@ namespace Helion.Util.Geometry.Boxes
         /// <param name="maxX">The top right X box coordinate.</param>
         /// <param name="maxY">The top right Y box coordinate.</param>
         /// <param name="maxZ">The top right Z box coordinate.</param>
-        public Box3D(double minX, double minY, double minZ, double maxX, double maxY, double maxZ) :
-            this(new Vec3D(minX, minY, minZ), new Vec3D(maxX, maxY, maxZ))
+        public Box3F(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) :
+            this(new Vec3F(minX, minY, minZ), new Vec3F(maxX, maxY, maxZ))
         {
         }
 
@@ -60,7 +67,7 @@ namespace Helion.Util.Geometry.Boxes
         /// </summary>
         /// <param name="min">The bottom left point.</param>
         /// <param name="max">The top right point.</param>
-        public Box3D(in Vec3D min, in Vec3D max)
+        public Box3F(in Vec3F min, in Vec3F max)
         {
             Min = min;
             Max = max;
@@ -73,13 +80,13 @@ namespace Helion.Util.Geometry.Boxes
         /// <param name="boxes">The boxes.</param>
         /// <returns>A box that encases all of the args tightly. If the list is
         /// empty, the result returned is the default generated box.</returns>
-        public static Box3D Combine(params Box3D[] boxes)
+        public static Box3F Combine(params Box3F[] boxes)
         {
             if (boxes.Empty())
                 return default;
 
-            (double minX, double minY, double minZ) = boxes[0].Min;
-            (double maxX, double maxY, double maxZ) = boxes[0].Max;
+            (float minX, float minY, float minZ) = boxes[0].Min;
+            (float maxX, float maxY, float maxZ) = boxes[0].Max;
 
             boxes.Skip(1).ForEach(box =>
             {
@@ -92,7 +99,7 @@ namespace Helion.Util.Geometry.Boxes
                 maxZ = Math.Max(maxZ, box.Max.Z);
             });
 
-            return new Box3D((minX, minY, minZ), (maxX, maxY, maxZ));
+            return new Box3F((minX, minY, minZ), (maxX, maxY, maxZ));
         }
 
         /// <summary>
@@ -101,6 +108,7 @@ namespace Helion.Util.Geometry.Boxes
         /// </summary>
         /// <param name="point">The point to check.</param>
         /// <returns>True if it is inside, false if not.</returns>
+        [Pure]
         public bool Contains(in Vec2D point)
         {
             return point.X <= Min.X || point.X >= Max.X || point.Y <= Min.Y || point.Y >= Max.Y;
@@ -112,7 +120,8 @@ namespace Helion.Util.Geometry.Boxes
         /// </summary>
         /// <param name="point">The point to check.</param>
         /// <returns>True if it is inside, false if not.</returns>
-        public bool Contains(in Vec3D point)
+        [Pure]
+        public bool Contains(in Vec3F point)
         {
             return !(point.X <= Min.X || point.X >= Max.X ||
                      point.Y <= Min.Y || point.Y >= Max.Y ||
@@ -125,7 +134,8 @@ namespace Helion.Util.Geometry.Boxes
         /// </summary>
         /// <param name="box">The other box to check against.</param>
         /// <returns>True if they overlap, false if not.</returns>
-        public bool Overlaps(in Box3D box)
+        [Pure]
+        public bool Overlaps(in Box3F box)
         {
             return !(Min.X >= box.Max.X || Max.X <= box.Min.X ||
                      Min.Y >= box.Max.Y || Max.Y <= box.Min.Y ||
@@ -133,5 +143,7 @@ namespace Helion.Util.Geometry.Boxes
         }
 
         public override string ToString() => $"({Min}), ({Max})";
+
+
     }
 }
--- Helion/Assets/Scripts/Helion/Worlds/Geometry/Subsectors/SubsectorPlane.cs
+++ Helion/Assets/Scripts/Helion/Worlds/Geometry/Subsectors/SubsectorPlane.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Helion.Unity;
+using Helion.Util.Geometry.Boxes;
 using Helion.Util.Geometry.Segments;
 using Helion.Util.Unity;
 using UnityEngine;
@@ -20,6 +21,8 @@ namespace Helion.Worlds.Geometry.Subsectors
 
         public SubsectorPlane(int index, SectorPlane sectorPlane, List<Seg2F> edges)
         {
+            Debug.Assert(edges.Count >= 3, $"Two or less lines is not a polygon (subsector plane {index})");
+
             string facingText = sectorPlane.IsCeiling ? "Ceiling" : "Floor";
 
             Index = index;
@@ -37,11 +40,39 @@ namespace Helion.Worlds.Geometry.Subsectors
             // TODO
         }
 
+        public bool IntersectedBy(in Box3F box)
+        {
+            Box2F box2D = box.XZ;
+
+            // Doing iteration by index to reduce GC pressure.
+            for (int i = 0; i < Edges.Count; i++)
+                if (Edges[i].Intersects(box2D))
+                    return HeightIntersectedBy(box);
+
+            // If we're not intersecting any lines, then we are either fully
+            // inside or fully outside. We can check this by taking any point
+            // (we'll use the bottom left corner) and seeing if it is on the
+            // right side of the first edge. Since the subsectors are clockwise
+            // then being on the right means it's inside (by definition).
+            if (Edges[0].OnRight(box.Min))
+                return HeightIntersectedBy(box);
+
+            return false;
+        }
+
         public void Dispose()
         {
             MeshComponents.Dispose();
             GameObjectHelper.Destroy(collisionInfo);
             GameObjectHelper.Destroy(gameObject);
+        }
+
+        private bool HeightIntersectedBy(in Box3F box)
+        {
+            // We consider intersections to not be touching, it has to fully
+            // cut through the height of the box.
+            // TODO: Will not play nicely with slopes...
+            return box.Min.Y < SectorPlane.Height && SectorPlane.Height < box.Max.Y;
         }
     }
 }
